57b748b8dd10edf53d35026560b2372b7dace1ae
Bleron Sylmetaj <bleronsylmetaj@91.life>
2025-02-13 14:47:26 +0100

    Added login form

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,68 @@ import MainContent from "./components/MainContent";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import "./App.css"
+import { useEffect, useState } from "react";
+import { Form, Input, message } from "antd";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    return sessionStorage.getItem('isLoggedIn') === 'true'; // Default to true or false from sessionStorage
+  });
+
+  useEffect(() => {
+    sessionStorage.setItem('isLoggedIn', isLoggedIn.toString());
+  }, [isLoggedIn]);
+
+  const handleLogin = (values) => {
+    setIsLoggedIn(true);
+    message.success("Login Success");
+  }
   return (
-    <div className="h-screen overflow-x-hidden overflow-y-auto">
-      <Navbar />
-      <Sidebar />
-      <MainContent />
-      {/* comment */}
-    </div>
+    <>
+      {!isLoggedIn && <div className="login-container h-screen flex justify-center items-center">
+        <Form
+          name="login-form"
+          initialValues={{ remember: true }}
+          onFinish={handleLogin}
+          className="login-form"
+          style={{ width: '300px' }}
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input placeholder="Username" />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password placeholder="Password" />
+          </Form.Item>
+
+          <Form.Item>
+            <button
+              style={{ backgroundColor: "rgba(0, 153, 153, 1)" }}
+              className="bg-green-500 px-4 py-2 rounded-lg text-white w-full"
+            >
+              Log in
+            </button>
+          </Form.Item>
+        </Form>
+      </div>
+      }
+      {
+        isLoggedIn && (
+          <div className="h-screen overflow-x-hidden overflow-y-auto">
+            <Navbar />
+            <Sidebar />
+            <MainContent />
+            {/* comment */}
+          </div>
+        )
+      }
+    </>
   );
 }
 
